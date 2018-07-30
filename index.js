9135aef9940a37bb1fea08993d6c045338378cfe
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-07-30 16:01:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,13 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+    
 client.on("message", (message) => {
     
-    if (isCommand(message, "-new")) {
+    if (isCommand(message, "تذكرة")) {
         const reason = message.content.split(" ").slice(1).join(" ");
         if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
-        if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`لا يمكنك عمل اكثر من تكت.`);
+        if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`لا يمكنك فتح اكثر من تذكرة واحدة`);
         message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
             let role = message.guild.roles.find("name", "Support Team");
             let role2 = message.guild.roles.find("name", "@everyone");
@@ -26,22 +27,23 @@ client.on("message", (message) => {
                 SEND_MESSAGES: true,
                 READ_MESSAGES: true
             });
-            message.channel.send(`:white_check_mark: تم صنع التكت, #${c.name}.`);
+            message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
             const embed = new Discord.RichEmbed()
                 .setColor(0xCF40FA)
-                .addField(`${message.author.username}!`, `سيتم مساعدتك من قبل فريق الدعم في اسرع وقت الرجاء الانتظار`)
+                .addField(`${message.author.username}!`, `تم فتح التذكرة وسيتم الرد عليك من قبل فريق المساعدة في اسرع وقت الرجاء الانتظار`)
                 .setTimestamp();
             c.send({
                 embed: embed
             });
-        }).catch(console.error); 
+        }).catch(console.error); // Send errors to console
     }
 
-
+    // Close ticket command
     if (isCommand(message, "اغلاق")) {
-        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`لا يمكنك غلق التيكت وانت خارجها`);
-
-        message.channel.send(`هل انت متأكد انك تريد حذف التيكت؟ لتأكيد اكتب **نعم** لديك 10 ثواني`)
+        if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`لا يمكنك اغلاق التكت وانت خارجها`);
+        // Confirm delete - with timeout (Not command)
+        message.channel.send(`هل انت متأكد؟ لتأكيد اكتب **نعم** لديك 10 ثواني لتأكيد
+            .then((m) => {
                 message.channel.awaitMessages(response => response.content === 'نعم', {
                         max: 1,
                         time: 10000,
@@ -51,11 +53,14 @@ client.on("message", (message) => {
                         message.channel.delete();
                     })
                     .catch(() => {
-                        m.edit('انته الوقت, لن يتم اغلاق التيكت').then(m2 => {
+                        m.edit('انته الوقت لن يتم اغلاق التيكت').then(m2 => {
                             m2.delete();
                         }, 3000);
                     });
+            });
+    }
 
+});
 client.on('message', message => {
   if (!message.content.startsWith(prefix)) return;
   var args = message.content.split(' ').slice(1);
