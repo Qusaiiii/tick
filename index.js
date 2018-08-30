87535b4836ae1499ce70a9796cd6f57b789d705f
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-08-30 17:26:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
     
 client.on("message", (message) => {
     
-    if (isCommand(message, "new")) {
+   if (message.content.startsWith("#new")) {
         const reason = message.content.split(" ").slice(1).join(" ");
         if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
         if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
@@ -39,12 +39,12 @@ client.on("message", (message) => {
     }
 
 
-    if (isCommand(message, "close")) {
+  if (message.content.startsWith("#close")) {
         if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
 
-        message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`/confirm\`. This will time out in 10 seconds and be cancelled.`)
+        message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`#confirm\`. This will time out in 10 seconds and be cancelled.`)
             .then((m) => {
-                message.channel.awaitMessages(response => response.content === '/confirm', {
+                message.channel.awaitMessages(response => response.content === '#confirm', {
                         max: 1,
                         time: 10000,
                         errors: ['time'],
